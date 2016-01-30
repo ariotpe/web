7435b825a48fa89a673977333793d46f0199d209
Frank Moreno <frankmoreno1993@gmail.com>
2016-01-30 15:01:28 -0500

    primera estructura de la web

---
+++ controllers/default.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"github.com/astaxie/beego"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (c *MainController) Get() {
+	c.Data["Website"] = "geckotronics.pe"
+	c.Data["Email"] = "ventas@geckotronics.com"
+	c.TplName = "index.tpl"
+}
