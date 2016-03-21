2e71212c730f01811511d917154072cf4ac19e60
Frank Moreno <frankmoreno1993@gmail.com>
2016-03-21 11:19:50 -0500

    inicio slider funciona pero falta atomizar

--- js/inicioCtrl.js
+++ js/inicioCtrl.js
@@ -1,32 +1,7 @@
 var appInicio = angular.module('myApp.Inicio',[]);
 
 appInicio.controller('inicioCtrl',function($scope){
-  $scope.sliderImgs = [
-    {
-      route: 'http://lorempixel.com/580/250/nature/1/',
-      align: 'center',
-      caption: 'Videovigilancia',
-      slogan: 'Supervisa tu casa desde donde quieras'
-    },
-    {
-      route: 'http://lorempixel.com/580/250/nature/2/',
-      align: 'left',
-      caption: 'Ahorro de energía',
-      slogan: 'Supervisa tu casa desde donde quieras'
-    },
-    {
-      route: 'http://lorempixel.com/580/250/nature/3',
-      align: 'right',
-      caption: 'Mayor Confort',
-      slogan: 'Controla tu casa desde botoneras inteligentes o tu teléfono móvil'
-    },
-    {
-      route: 'http://lorempixel.com/580/250/nature/4',
-      align: 'center',
-      caption: 'Energías renovables',
-      slogan: 'Instalación de paneles solares'
-    }
-  ];
+
 });
 
 appInicio.directive('slider', function($document) {
@@ -35,17 +10,7 @@ appInicio.directive('slider', function($document) {
     scope: {
       images: '='
     },
-    /*template: '<ul class="slides">'
-      + '<li ng-repeat="img in images">'
-        + '<img src="{{img.route}}">'
-        + '<div class="caption {{img.align}}-align">'
-         + '<h3>{{img.caption}}</h3>'
-         + '<h5 class="light grey-text text-lighten-3">{{img.slogan}}</h5>'
-        + '</div>'
-      + '</li>'
-    + '</ul>',*/
     template: str,
-
     /*templateUrl: 'base/slider.html',*/
     link: linkFunc
   };
@@ -79,15 +44,15 @@ appInicio.directive('slider', function($document) {
 
   var str = '<ul class="slides">';
   for (i = 0; i < sliderImgs.length; i++) {
-        var obj = sliderImgs[i];
-        str += '<li><img class="responsive-img" src="'+obj.route+'">';
-        str += '  <div class="caption '+obj.align+'-align">';
-        str += '    <h3>'+obj.caption+'</h3>';
-        str += '    <h5 class="light grey-text text-lighten-3">'+obj.slogan+'</h5>';
-        str += '  </div>';
-        str += '</li>';
-   };
-    str += '</ul>';
+    var obj = sliderImgs[i];
+    str += '<li><img class="responsive-img" src="'+obj.route+'">';
+    str += '  <div class="caption '+obj.align+'-align">';
+    str += '    <h3>'+obj.caption+'</h3>';
+    str += '    <h5 class="light grey-text text-lighten-3">'+obj.slogan+'</h5>';
+    str += '  </div>';
+    str += '</li>';
+  };
+  str += '</ul>';
     $('#GeckoCarousel').html(str);
 
   function linkFunc(scope, elem, attr) {
