4e555744927d97464c4d1fd72a0ed609d98c5355
Frank Moreno <frankmoreno1993@gmail.com>
2016-03-23 16:19:33 -0500

    Mejoras en el inicio

--- js/inicioCtrl.js
+++ js/inicioCtrl.js
@@ -1,35 +1,13 @@
 var appInicio = angular.module('myApp.Inicio',[]);
 
-appInicio.controller('inicioCtrl',function($scope){
-
-});
+appInicio.controller('inicioCtrl',function($scope){ });
 
 var sliderImgs = [
-  {
-    'route': 'http://lorempixel.com/580/250/nature/1/',
-    'align': 'center',
-    'caption': 'Videovigilancia',
-    'slogan': 'Supervisa tu casa desde donde quieras'
-  },
-  {
-    'route': 'http://lorempixel.com/580/250/nature/2/',
-    'align': 'left',
-    'caption': 'Ahorro de energía',
-    'slogan': 'Supervisa tu casa desde donde quieras'
-  },
-  {
-    'route': 'http://lorempixel.com/580/250/nature/3',
-    'align': 'right',
-    'caption': 'Mayor Confort',
-    'slogan': 'Controla tu casa desde botoneras inteligentes o tu teléfono móvil'
-  },
-  {
-    'route': 'http://lorempixel.com/580/250/nature/4',
-    'align': 'center',
-    'caption': 'Energías renovables',
-    'slogan': 'Instalación de paneles solares'
-  }
-];
+    'https://raw.githubusercontent.com/geckotronics/webcontent/master/img/inicio/domotica.png',
+    'https://raw.githubusercontent.com/geckotronics/webcontent/master/img/inicio/inmotica.png',
+    'https://raw.githubusercontent.com/geckotronics/webcontent/master/img/inicio/seguridadelectronica.png',
+    'https://raw.githubusercontent.com/geckotronics/webcontent/master/img/inicio/solarpanels.png'
+  ];
 
 appInicio.directive('slider', function($document) {
   var directive = {
@@ -42,15 +20,9 @@ appInicio.directive('slider', function($document) {
     link: linkFunc
   };
 
-  var str = '<ul class="slides responsive-img">';
+  var str = '<ul class="slides">';
   for (i = 0; i < sliderImgs.length; i++) {
-    var obj = sliderImgs[i];
-    str += '<li><img src="'+obj.route+'">';
-    str += '  <div class="caption '+obj.align+'-align">';
-    str += '    <h3>'+obj.caption+'</h3>';
-    str += '    <h5 class="light grey-text text-lighten-3">'+obj.slogan+'</h5>';
-    str += '  </div>';
-    str += '</li>';
+    str += '<li><img src="'+sliderImgs[i]+'"></li>';
   };
   str += '</ul>';
   $('#GeckoCarousel').html(str);
